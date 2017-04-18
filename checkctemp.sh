6e6b284876ed3f5670219310975e3a14598ef9b0
uwslowcontrol <uwslowcontrol@minard.sp.snolab.ca>
2017-04-18 16:29:56 -0400

    Bash script that checks if temperature sensor reading script is alive

---
+++ checkctemp.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#To get mail and see errors when trying to cavitytemps.py from this, 
+#Remove MAILTO='' in the cronjob
+MAILTO=""
+RUNNING=`ps -ef | grep cavitytemps.py | grep python`
+if [ "$RUNNING" == "" ]
+then
+  echo "cavitytemps script is not running on `hostname`!"
+  cd /home/uwslowcontrol/pi_db/cavitytemp/
+  python /home/uwslowcontrol/pi_db/cavitytemp/cavitytemps.py &
+  SUBJECT="cavitytemps script restarted on `hostname`"
+  MESSAGE="cavitytemps script restarted at `date` as `ps -ef | grep cavitytemps`"
+  echo $MESSAGE | cat >> ~/pi_db/log/cavitytemp.log
+  echo "cavitytemps script is started on `hostname`."
+else
+  echo "cavitytemps script is running on `hostname`."
+fi
