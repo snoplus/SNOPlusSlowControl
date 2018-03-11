dda9dc3d96bbea916024f286a4b4651a59de6154
uwslowcontrol <uwslowcontrol@minard.sp.snolab.ca>
2018-03-11 13:20:47 -0400

    cavitytemps now reads from /raid/CavityTemperature/temperature.log

--- cavitytemp/cavitytemps.py
+++ cavitytemp/cavitytemps.py
@@ -5,7 +5,7 @@
 #Written by: Teal Pershing
 #Last Updated: 03/20/2017
 
-import subprocess
+import lib.dataio as dio
 import lib.couchutils as cu
 import lib.tempreader as tr
 import lib.alarmposter as ap
@@ -15,9 +15,8 @@ import time
 
 DEBUG = False
 ERR_SLEEPTIME = 60
-SUCCESS_SLEEPTIME = 360
-
-
+NONEWDATA_SLEEPTIME = 120
+SUCCESS_SLEEPTIME = 20
 
 # --------- Logger configuration ----------- #
 CLOG_FILENAME = '/home/uwslowcontrol/pi_db/log/cavitytemp.log' #logfile source
@@ -34,37 +33,12 @@ def UE_handler(exec_type, value, tb):
 
 #At an uncaught exception, run our handler
 sys.excepthook = UE_handler
-
-# ------- /Logger configuration --------- #
-
-#Sends command to temp reader, returns raw read data
-def getReading():
-    timedelay = '1'
-    numreads = '1'  #Set to zero to forever loop
-    sport = '/dev/ttyUSB0' #Location of serial port
-    comm = ['digitemp_DS9097U','-i','-a','-d',timedelay,
-        '-n',numreads,'-s',sport]
-    #This is deprecated!  But we're stuck at 2.6.6 so...
-    tempreading = subprocess.Popen(comm, stdout=subprocess.PIPE).communicate()[0]
-    return tempreading
-
-#Save readings to text file for Noel
-def WriteReadToLog(TempReader):
-    SensorReads = reader.getSensorLines()
-    if SensorReads is not None:
-        f = open("TempReadings.log","a")
-        f.write("\n")
-        for sensorline in SensorReads:
-            combinedline = " ".join(sensorline)
-            if DEBUG == True:
-                print("LINE WRITEN TO LOG: " + combinedline)
-            f.write(combinedline + "\n")
-        f.close()
-
    
 # --------- END Logger configuration -------- #
 
 if __name__ == "__main__":
+    temperature_file = '/raid/CavityTemperature/temperature.log'
+    numsensors = 30
     channeldb = None
     DATA_LABEL = "temp_sensors"
     channeldb = cu.getChannelParameters(channeldb)
@@ -72,6 +46,7 @@ if __name__ == "__main__":
     AlarmControl.set_alarmid(30040)
     AlarmControl.set_datatype(DATA_LABEL)
     AlarmControl.set_sensorkey("Sensor")
+    old_logread = None
     while True:
         #Update thresholds in case they were changed on webpage
         channeldb_last = channeldb
@@ -79,13 +54,21 @@ if __name__ == "__main__":
         AlarmControl.setCurrentChanneldb(channeldb)
 
         #Get the current reading from the sensors
-        rawread = getReading()
-        rawread = rawread.splitlines()
-        reader = tr.TempReader(rawread)
+        #rawread = dio.getReading()
+        #rawread = rawread.splitlines()
+
+	#Or, read from a log file already being ouptut
+        logread = dio.getFileTail(temperature_file,30)
+        if old_logread is not None:
+            if logread==old_logread:
+                logging.info("No new data in file.  Try again in "+str(NONEWDATA_SLEEPTIME)+" sec.")
+                time.sleep(NONEWDATA_SLEEPTIME)
+        reader = tr.TempReader(logread)
         reader.set_dicttype(DATA_LABEL)
         reader.settime(int(time.time()))
         reader.setunit("(C)")
         reader.parseTemps()
+        reader.show()
         if reader.hasrawvalues == False:
             logging.info("No values grabbed from sensor. Sensor" + \
                 " may have been polled by another source at this time.")
@@ -100,8 +83,9 @@ if __name__ == "__main__":
             #values parsed. check for values outside threshold and alarm
             AlarmControl.updateCurrentValues(reader.readingdict)
             AlarmControl.checkForAlarms() #Posts alarms if any
-            WriteReadToLog(reader)
+            #dio.WriteReadToLog(reader)
             #save the dictionary to couchDB
             cu.saveValuesToCT(reader.readingdict)
+            old_logread = logread
             time.sleep(SUCCESS_SLEEPTIME)
 
