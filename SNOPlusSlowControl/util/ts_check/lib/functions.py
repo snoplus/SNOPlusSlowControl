57d0175e44a9bdbf9ebe6432337fcd14908db213
Tianqi Zhang <tqzhang@ucdavis.edu>
2019-04-18 03:57:44 -0400

    removed the redundant import of function.py removed the redundant daughter class of couchutil.py removed log info

--- SNOPlusSlowControl/util/ts_check/lib/functions.py
+++ SNOPlusSlowControl/util/ts_check/lib/functions.py
@@ -1,15 +1,5 @@
 from __future__ import print_function
-import couchdb
-import sys
-import string, calendar, time, urllib2, base64
-import lib.config.tsconfig as c
-import lib.alarm_server as als
-import lib.getcreds as gc
-import lib.emailsend as es
 import TimestampComparer as ts
-
-from email import utils
-import logging
 
 def getCavityTempsDocAge(LatestEntry):
     comparer = ts.TimestampComparer(LatestEntry)
